19a5b672cc13789bbbe87c2d95d2905306c63394
shinyPIKACHUx <mcskyx117@gmail.com>
2016-01-29 08:25:18 -0600

    updated to reflect the template added the thing at the very top

--- src/CannedAppleSauce.java
+++ src/CannedAppleSauce.java
@@ -1,18 +1,13 @@
+package interface.food;
 
 public class CannedAppleSauce extends Food{
 	public CannedAppleSauce {
 		setCost(.7);
-		getCost();
 		changeCost(.5, 7);
 		setStoreCost(.2);
-		getStoreCost();
 		setExpiration(365);
-		getExpiration();
 		setDesirability(0);
-		getDesirability();
 		setName("Canned Apple Sauce");
-		String getName();
 		setQuantity(100);
-		getQuantity();
 	}
 }
--- src/CannedApples.java
+++ src/CannedApples.java
@@ -1,18 +1,13 @@
+package interface.food;
 
 public class CannedApples extends Food{
 	public CannedApples {
 		setCost(.5);
-		getCost();
 		changeCost(.25, 7);
 		setStoreCost(.1);
-		getStoreCost();
 		setExpiration(365);
-		getExpiration();
 		setDesirability(0);
-		getDesirability();
 		setName("Canned Apples");
-		String getName();
 		setQuantity(100);
-		getQuantity();
 	}
 }
